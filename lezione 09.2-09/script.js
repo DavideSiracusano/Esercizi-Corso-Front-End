40b6a7ee3903baa61257fdcec6ac0e14009e1c94
Davide Siracusano <152174921+DavideSiracusano@users.noreply.github.com>
2025-09-09 15:12:32 +0200

    esercizi array

---
+++ lezione 09.2-09/script.js
@@ -0,0 +1,22 @@
+//stampa i colori
+let array = ["rosso", "verde", "blu"];
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
+
+//aggiungi giallo e arancione
+let array1 = ["rosso", "verde", "blu"];
+array1.push("giallo", "arancione");
+console.log(array1);
+
+//se include verde, stampa l'indice del colore verde
+if (array1.includes("verde")) {
+  console.log(array1.indexOf("verde"));
+}
+
+//cambia il colore seconda posizione
+array1[1] = "viola";
+console.log(array1);
+
+array1.shift();
+console.log(array1);
